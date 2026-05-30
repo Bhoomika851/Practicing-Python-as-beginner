e82e9b918f9bda8be67273886b82a2bfb340296a
Bhoomika <bhu85123@gmail.com>
2026-05-30 23:54:46 +0530

    Add basic arithmetic operations in py
    Implement basic arithmetic operations and print results.

---
+++ Practice2.py
@@ -0,0 +1,31 @@
+#Operations in python
+a = 25
+b = 5
+sum = a + b         #addition
+print(sum)          #30
+
+sub = a - b         #subtraction
+print(sub)          #20
+
+product = a*b       #Multiplication
+print(product)      #125
+
+divide = a/b        #Normal division
+print(divide)       #5.0
+divide_int = a//b   #Division with integer output
+print(divide_int)   #5
+
+exp = 30 + 2 * 4
+print(exp)          #38
+exp = (30 + 2)* 4
+print(exp)          #128
+
+a/=5
+print(a)            #5.0
+
+x = 3+2*2           #7
+print(x)
+y=(3+2)*2           #10
+print(y)
+z=x+y               #17
+print(z)
