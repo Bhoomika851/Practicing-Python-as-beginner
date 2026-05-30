561643e08fe0d4a9a9960a8874d3e58d765af166
Bhoomika <bhu85123@gmail.com>
2026-05-30 22:58:31 +0530

    Add Data types and conversions in python
    Demonstrate data types and conversions in Python.

---
+++ Practice1.py
@@ -0,0 +1,21 @@
+print("Hello")
+
+#Check all 4 data types
+type(2.5)
+print(type(2.5))                 #float
+type(2)
+print(type(2))                   #int
+a = type("Hell No")
+print(a)                         #string
+print(type("hello"=="world"))    #bool
+
+#convert data types
+print(int(3.54))       #float to integer
+print(str(5.4))        #float to string
+print(bool(True))
+
+print(float(4))        #int to float
+print(str(7))          #int to str
+
+print(int("5"))        #str to int
+print(float("6.7"))    #str to float
